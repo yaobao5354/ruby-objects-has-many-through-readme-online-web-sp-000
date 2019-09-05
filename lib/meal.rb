e23e420379cff980223eb1f87ae18693718b3a58
yaoyu bao <yaobao5354@gmail.com>
2019-09-05 05:26:25 +0000

    Automatically backed up by Learn

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,12 +1,18 @@
 class Meal
+  
+  @@all = []
 
   def initialize(waiter, customer, total, tip)
     @waiter = waiter
     @customer = customer
     @total = total
     @tip = tip
+    @@all << self
   end
   
+  def self.all 
+    @@all 
+  end 
   
 
 end
